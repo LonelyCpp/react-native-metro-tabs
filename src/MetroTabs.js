6769906b701fa2084976ed1e0b38d42a7ee1b880
Ananthu Kanive <ananthu.kanive@smallcase.com>
2020-07-12 19:26:01 +0530

    rewrite animations in reanimated

--- src/MetroTabs.js
+++ src/MetroTabs.js
@@ -1,124 +1,133 @@
-import React, { Component } from "react";
-import {
-  FlatList,
-  StyleSheet,
-  Text,
-  View,
-  Dimensions,
-  TouchableOpacity,
-  Animated,
-  Easing
-} from "react-native";
+import React, { useRef, useCallback } from "react";
+import Animated from "react-native-reanimated";
+import { View, StyleSheet, Dimensions, TouchableOpacity } from "react-native";
+import { useValue, onScrollEvent, interpolateColor } from "react-native-redash";
 
-export default class MetroTabs extends Component {
-  WINDOW_WIDTH = Dimensions.get("window").width;
-  HEADER_WIDTH = this.WINDOW_WIDTH / 1.7;
+const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("screen");
 
-  state = {
-    currentPage: "1",
-    headerOffset: new Animated.Value(0)
-  };
+// header width is 1.7 times smaller than the screen
+const HEADER_ITEM_WIDTH = SCREEN_WIDTH / 1.7;
 
-  render() {
-    console.log(this.props.screens);
-    return (
-      <View style={styles.container}>
-        <FlatList
-          contentContainerStyle={styles.screenList}
-          ref={screenList => (this.screenList = screenList)}
-          horizontal
-          snapToAlignment={"center"}
-          decelerationRate={"fast"}
-          snapToInterval={this.WINDOW_WIDTH - 10}
-          data={this.props.screens}
-          scrollEventThrottle={50}
-          onScroll={this.handleScroll}
-          showsHorizontalScrollIndicator={false}
-          renderItem={({ item }) => (
-            <View style={styles.screenContainer}>{item.screen}</View>
-          )}
-          onViewableItemsChanged={this.onViewableItemsChanged}
-        />
-        <Animated.View
-          style={[
-            styles.tabContainer,
-            { transform: [{ translateX: this.state.headerOffset }] }
-          ]}
-        >
-          {this.props.screens.map(item => {
-            return (
-              <TouchableOpacity onPress={() => this.headerPress(item)}>
-                <Text
-                  style={[
-                    styles.tabText,
-                    { width: this.HEADER_WIDTH },
-                    this.state.currentPage === item.key && styles.tabTextActive
-                  ]}
-                >
-                  {item.title}
-                </Text>
-              </TouchableOpacity>
-            );
-          })}
-        </Animated.View>
-      </View>
-    );
-  }
+// slightly smaller snap to value to make the next screen peep out
+const SCREEN_SNAP_INTERVAL = SCREEN_WIDTH - 20;
 
-  handleScroll = ({
-    nativeEvent: {
-      contentOffset: { x }
-    }
-  }) => {
-    const { headerOffset } = this.state;
-    const toValue = (x * this.HEADER_WIDTH) / -this.WINDOW_WIDTH;
-    headerOffset.stopAnimation();
-    Animated.timing(headerOffset, {
-      toValue,
-      duration: 100,
-      easing: Easing.ease,
-      useNativeDriver: true
-    }).start();
-  };
+/**
+ * @param {Object} props
+ * @param {Array} props.screens
+ */
+const MetroTabs = (props) => {
+  const { screens } = props;
 
-  onViewableItemsChanged = info => {
-    if (info.viewableItems.length >= 1) {
-      this.setState({ currentPage: info.viewableItems[0].key });
-      console.log("scrolling to ", info.viewableItems[0].index);
-    }
-  };
+  const scrollViewRef = useRef(null);
 
-  headerPress = item => {
-    this.setState({ currentPage: item.key });
-    this.screenList.scrollToItem({ item: { key: item } });
-  };
-}
+  // main animated node
+  // everything else is interpolated on this node
+  const scrollViewX = useValue(0);
+
+  // onScroll boilerplate from redash
+  const onScroll = onScrollEvent({ x: scrollViewX });
+
+  // does the actual parallax interpolation
+  const headerX = Animated.interpolate(scrollViewX, {
+    // max scrolled value for scroll-view
+    inputRange: [0, SCREEN_SNAP_INTERVAL * screens.length],
+    // max parallax translate for the header
+    // negative because the header is translated to the left
+    outputRange: [0, HEADER_ITEM_WIDTH * screens.length * -1],
+  });
+
+  // scrolls to the correct screen when one of the tabs are pressed
+  const onTabPress = useCallback(async (index) => {
+    scrollViewRef.current
+      ?.getNode()
+      .scrollTo({ x: index * SCREEN_SNAP_INTERVAL, animated: true });
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Animated.ScrollView
+        horizontal
+        bounces={false}
+        {...{ onScroll }}
+        ref={scrollViewRef}
+        scrollEventThrottle={16}
+        decelerationRate={"fast"}
+        snapToInterval={SCREEN_SNAP_INTERVAL}
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.screenList}
+      >
+        {screens.map((item) => (
+          <View style={styles.screenContainer}>{item.screen}</View>
+        ))}
+      </Animated.ScrollView>
+      <Animated.View
+        style={[styles.tabContainer, { transform: [{ translateX: headerX }] }]}
+      >
+        {screens.map((item, index) => (
+          <HeaderItem
+            item={item}
+            index={index}
+            onPress={onTabPress}
+            maxLen={screens.length}
+            scrollViewX={scrollViewX}
+          />
+        ))}
+      </Animated.View>
+    </View>
+  );
+};
+
+const activeColor = "#ffffff";
+const inActiveColor = "#333333";
+
+const HeaderItem = ({ item, index, maxLen, scrollViewX, onPress }) => {
+  const color = interpolateColor(scrollViewX, {
+    // array of snap intervals
+    inputRange: new Array(maxLen)
+      .fill(0)
+      .map((_, i) => SCREEN_SNAP_INTERVAL * i),
+
+    // generate a color array with active color in the index position
+    outputRange: new Array(maxLen)
+      .fill(0)
+      .map((_, i) => (i === index ? activeColor : inActiveColor)),
+  });
+
+  return (
+    <TouchableOpacity onPress={() => onPress(index)}>
+      <Animated.Text style={[styles.tabText, { color }]}>
+        {item.title}
+      </Animated.Text>
+    </TouchableOpacity>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: 120,
-    backgroundColor: "black"
+    backgroundColor: "black",
   },
   screenContainer: {
-    height: Dimensions.get("window").height,
-    width: Dimensions.get("window").width - 20
+    height: SCREEN_HEIGHT,
+    width: SCREEN_SNAP_INTERVAL,
   },
   screenList: {
-    paddingEnd: 20
+    paddingEnd: 20,
   },
   tabContainer: {
+    top: 8,
     position: "absolute",
-    top: 0,
-    flexDirection: "row"
+    flexDirection: "row",
   },
   tabText: {
-    color: "white",
     padding: 10,
-    marginTop: 20,
     fontSize: 50,
-    opacity: 0.4
+    marginTop: 20,
+    width: HEADER_ITEM_WIDTH,
   },
   tabTextActive: {
-    opacity: 1
-  }
+    opacity: 1,
+  },
 });
+
+export default MetroTabs;
